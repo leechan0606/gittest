5cc345407ab7ac680113f507017fa395a64dfbd3
leechan0606 <578007025@qq.com>
2021-11-28 23:36:07 +0800

    添加文件

---
+++ src/main/java/com/leechan/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.leechan.git;
+
+/**
+ * @author leechan
+ * @date 2021/11/28 23:10
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
